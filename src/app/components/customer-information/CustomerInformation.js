3e5f448bb635f53072529991d5ae08054bc34bc7
Jakob Wilmesmeier <Jakob.Wilmesmeier@hsw-stud.de>
2022-02-11 16:00:16 +0100

    folder structure

---
+++ src/app/components/customer-information/CustomerInformation.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './CustomerInformation.css';
+
+export default function CustomerInformation() {
+    return <>
+        <div className="customerContainer">
+            
+        </div>
+    </>;
+}
