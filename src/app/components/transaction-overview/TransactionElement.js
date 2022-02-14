22c0d9cec454fc799588b8b857196b10926db02d
Lars Kecker <lars.kecker2009@gmail.com>
2022-02-14 16:14:09 +0100

    hide transaction icon on mobile devices

--- src/app/components/transaction-overview/TransactionElement.js
+++ src/app/components/transaction-overview/TransactionElement.js
@@ -1,40 +1,53 @@
 import BusinessIcon from '@mui/icons-material/Business';
-import { Avatar, Box, ListItem, ListItemAvatar, ListItemButton, Typography } from "@mui/material";
-import React from "react";
+import { Avatar, Box, Divider, ListItem, ListItemAvatar, ListItemButton, Typography, useMediaQuery } from "@mui/material";
+import React, { useState } from "react";
 
 const currencyFormat = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' });
 
 const TransactionElement = ({ transaction, isLast }) => {
+
+    // hide the icon on small devices
+    const bigDevice = useMediaQuery('(min-width:600px)');
+
+    const [open, setOpen] = useState(false);
+
     return (
-        <ListItem sx={{ padding: 0, borderBottom: '1px dotted #9da3a6' }}>
-            <ListItemButton>
-                <ListItemAvatar>
-                    <Avatar>
-                        <BusinessIcon />
-                    </Avatar>
-                </ListItemAvatar>
-                <Box sx={{ padding: '12px 32px 16px 24px', width: '100%' }}>
-                    <Box sx={{ display: 'flex', width: '100%' }}>
-                        <Typography variant='h6' sx={{ display: 'inline', flexGrow: 1, fontWeight: 'bold' }}>
-                            {transaction.name}
+        <>
+            <ListItem sx={{ padding: 0 }}>
+                <ListItemButton onClick={() => setOpen(true)}>
+                    {bigDevice && (
+                        <ListItemAvatar sx={{marginRight: '12px'}}>
+                            <Avatar>
+                                <BusinessIcon />
+                            </Avatar>
+                        </ListItemAvatar>)}
+                    <Box sx={{
+                        padding: '12px 32px 16px 12px',
+                        width: '100%'
+                    }}>
+                        <Box sx={{ display: 'flex', width: '100%' }}>
+                            <Typography variant='h6' sx={{ display: 'inline', flexGrow: 1, fontWeight: 'bold', marginRight: '12px' }}>
+                                {transaction.name}
+                            </Typography>
+                            <Typography align='right' sx={{ display: 'inline', fontWeight: 'bold' }}>
+                                {currencyFormat.format(transaction.totalPrice)}
+                            </Typography>
+                        </Box>
+
+                        <Typography variant='subtitle1'>
+                            {transaction.date}
                         </Typography>
-                        <Typography align='right' sx={{ display: 'inline', fontWeight: 'bold' }}>
-                            {currencyFormat.format(transaction.totalPrice)}
+                        <Typography variant='body1'>
+                            Anzahl: {transaction.count}
+                        </Typography>
+                        <Typography variant='body1'>
+                            Preis pro Stück: {currencyFormat.format(transaction.pricePerShare)}
                         </Typography>
                     </Box>
-
-                    <Typography variant='subtitle1'>
-                        {transaction.date}
-                    </Typography>
-                    <Typography variant='body1'>
-                        Anzahl: {transaction.count}
-                    </Typography>
-                    <Typography variant='body1'>
-                        Preis pro Stück: {currencyFormat.format(transaction.pricePerShare)}
-                    </Typography>
-                </Box>
-            </ListItemButton>
-        </ListItem>
+                </ListItemButton>
+            </ListItem>
+            {!isLast && <Divider />}
+        </>
     )
 };
 
--- src/app/components/transaction-overview/TransactionOverview.js
+++ src/app/components/transaction-overview/TransactionOverview.js
@@ -28,6 +28,54 @@ const TransactionOverview = () => {
             pricePerShare: 2672,
             totalPrice: -2672,
             date: '03.07.2021',
+        },
+        {
+            id: 4,
+            name: "Alphabet",
+            count: 4,
+            pricePerShare: 2350,
+            totalPrice: -9400,
+            date: '06.12.2021',
+        },
+        {
+            id: 5,
+            name: "Meta",
+            count: 1,
+            pricePerShare: 192,
+            totalPrice: -192,
+            date: '01.01.2021',
+        },
+        {
+            id: 6,
+            name: "Amazon.com, Inc.",
+            count: 1,
+            pricePerShare: 2672,
+            totalPrice: -2672,
+            date: '03.07.2021',
+        },
+        {
+            id: 7,
+            name: "Alphabet",
+            count: 4,
+            pricePerShare: 2350,
+            totalPrice: -9400,
+            date: '06.12.2021',
+        },
+        {
+            id: 8,
+            name: "Meta",
+            count: 1,
+            pricePerShare: 192,
+            totalPrice: -192,
+            date: '01.01.2021',
+        },
+        {
+            id: 9,
+            name: "Amazon.com, Inc.",
+            count: 1,
+            pricePerShare: 2672,
+            totalPrice: -2672,
+            date: '03.07.2021',
         }
     ]
 
@@ -38,7 +86,11 @@ const TransactionOverview = () => {
     >
         <List>
             {transactions.map((transaction, index) => (
-                <TransactionElement key={transaction.id} transaction={transaction} isLast={index === transactions.length - 1} />
+                <TransactionElement
+                    key={transaction.id}
+                    transaction={transaction}
+                    isLast={index === transactions.length - 1}
+                />
             ))}
         </List>
     </Container>
