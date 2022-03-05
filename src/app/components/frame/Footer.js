ffedf11868e2fae114b16aee25d0f6b4239aa54d
com2000 <comm.cody2000@gmail.com>
2022-03-05 10:19:27 +0100

    add darkMode in Footer

--- src/app/components/frame/Footer.js
+++ src/app/components/frame/Footer.js
@@ -1,26 +1,59 @@
 import { AppBar, Link, Grid, Toolbar, Typography } from '@mui/material';
 import "./Footer.css"
-const Footer = () => {
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 
-    return (
-        <AppBar position="static">
-            <Toolbar>
-                <Grid container spacing={1}>
-                    <Grid item xs={10}>
-                        <Typography component="div" sx={{ flexGrow: 1 }}>
-                            © HSW-Hameln
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <Link href="#">Impressum</Link>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <Link href="#">DSGVO</Link>
-                    </Grid>
-                </Grid>
-            </Toolbar>
-        </AppBar>
-    )
+const Footer = () => {
+  const [mode, setMode] = React.useState('light');
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+
+  return (
+    
+    <ThemeProvider theme={theme}>
+    <AppBar position="static">
+      <Toolbar>
+        <Grid container spacing={1}>
+          <Grid item xs={10}>
+            <Typography component="div" sx={{ flexGrow: 1 }}>
+              © HSW-Hameln
+            </Typography>
+          </Grid>
+          <Grid item xs={1}>
+            <Link href="#">Impressum</Link>
+          </Grid>
+          <Grid item xs={1}>
+            <Link href="#">DSGVO</Link>
+          </Grid>
+          <Grid>
+            <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+              {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+    </ThemeProvider>
+  )
 }
 
 export default Footer;
