883be576534fd8f23c3bc3635bfb310b06ba2254
Jakob <j.wilmesmeier@gmail.com>
2022-03-05 12:17:09 +0100

    implemented buy sell api

--- src/app/components/trading/BuySellDialog.js
+++ src/app/components/trading/BuySellDialog.js
@@ -1,8 +1,10 @@
 import { Alert, Button, Dialog, DialogContentText, DialogTitle, Snackbar, TextField } from "@mui/material";
 import { purple } from "@mui/material/colors";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import InputMask from 'react-input-mask';
+import { DepotContext } from "../../../context/DepotContext";
+import { UserContext } from "../../../context/UserContext";
 import useDepot from "../../hooks/useDepot";
 import useUser from "../../hooks/useUser";
 
@@ -16,27 +18,47 @@ export default function BuySellDialog({stock, isOpen, handleClose}) {
     const [method, setMethod] = useState("");
     const [errorMsg, setErrorMsg] = useState("");
     const [isError, setIsError] = useState(false);
+    const [ownedAmount, setOwnedAmount] = useState("");
 
     function closeDialog() {
         setAmount("");
         handleClose();
     }
 
-    const userValue = useUser();
-    const depotValue = useDepot(userValue.currentUser?.client_id);
+    const { currentUser } = useContext(UserContext);
+    const { currentDepot } = useContext(DepotContext);
+
+    useEffect(() => {
+        const fetchStock = async () => {
+            axios.get(`http://localhost:8081/depots/${currentDepot.position_id}/${currentDepot.position_sub_id}/currentStocks`).then(res => {
+                res.data.forEach(fetchedStock => {
+                    if (fetchedStock.isin === stock.isin) {
+                        setOwnedAmount(stock.piece_amt);
+                    }
+                });
+            });
+        };
+        fetchStock();
+    }, [stock]);
+    
 
     function buyStock() {
         setMethod("buy");
         if (amount) {
             //TODO set position and sub id correctly
-            axios.post(`http://localhost:8081/orders/${depotValue.currentDepot.position_id}/${depotValue.currentDepot.position_sub_id}`, {stock_isin: stock.isin, amount: parseInt(amount)}).then((res) => {
-                if (res.status === 200) {
-                    setSuccessOpen(true);
-                    stock.amount = stock.amount + parseInt(amount);
-                } else {
-                    //TODO error handling
-                }
-            });
+            axios.post(`http://localhost:8081/orders/${currentDepot.position_id}/${currentDepot.position_sub_id}`, 
+                {
+                    stock_isin: stock.isin, 
+                    amount: parseInt(amount), 
+                    price_per_stock: stock.price_per_stock}
+                ).then((res) => {
+                    if (res.status === 200) {
+                        setSuccessOpen(true);
+                        stock.amount = stock.amount + parseInt(amount);
+                    } else {
+                        //TODO error handling
+                    }
+                });
         } else {
             setIsError(true);
         }
@@ -45,16 +67,20 @@ export default function BuySellDialog({stock, isOpen, handleClose}) {
     function sellStock() {
         setMethod("sell");
         if (amount) {
-            if (stock.amount >= parseInt(amount)) {
-                //TODO set position and sub id correctly
-            axios.post(`http://localhost:8081/orders/${depotValue.currentDepot.position_id}/${depotValue.currentDepot.position_sub_id}`, {stock_isin: stock.isin, amount: parseInt(amount * -1)}).then((res) => {
-                if (res.status === 200) {
-                    setSuccessOpen(true);
-                    stock.amount = stock.amount - parseInt(amount);
-                } else {
-                    //TODO error handling
-                }
-            });
+            if (ownedAmount >= parseInt(amount)) {
+            axios.post(`http://localhost:8081/orders/${currentDepot.position_id}/${currentDepot.position_sub_id}`, 
+                {
+                    stock_isin: stock.isin, 
+                    amount: parseInt(amount * -1), 
+                    price_per_stock: stock.price_per_stock
+                }).then((res) => {
+                    if (res.status === 200) {
+                        setSuccessOpen(true);
+                        setOwnedAmount(ownedAmount - parseInt(amount));
+                    } else {
+                        //TODO error handling
+                    }
+                });
             } else {
                 setErrorMsg("Nicht genügend Wertpapiere vorhanden");
                 setErrorOpen(true);
@@ -82,8 +108,8 @@ export default function BuySellDialog({stock, isOpen, handleClose}) {
             <svg style={{maxWidth: '90px', maxHeight: '50px', marginLeft: 'auto', marginRight: 'auto', marginBottom: '20px'}}>
                         <image href={stock.link} style={{width: '90px', transform: 'translateY(25%'}}></image>
             </svg>
-            <DialogContentText className="tradingContent">{`Aktueller Preis: ${stock.price_per_stock}€`}</DialogContentText>
-            <DialogContentText className="tradingContent">{`Anzahl im Besitz: ${stock.amount}`}</DialogContentText>
+            <DialogContentText className="tradingContent">{`Aktueller Preis: ${Number(stock.price_per_stock).toFixed(2)}€`}</DialogContentText>
+            <DialogContentText className="tradingContent">{`Anzahl im Besitz: ${ownedAmount}`}</DialogContentText>
             <div className="amount">
                 <DialogContentText className="tradingContent amountLabel">Anzahl zu handeln: </DialogContentText>
                 <InputMask 
@@ -99,7 +125,7 @@ export default function BuySellDialog({stock, isOpen, handleClose}) {
                 </InputMask>
             </div>
             { amount.length > 0 &&
-                <DialogContentText className="tradingContent" sx={{marginTop: '10px'}}>{`Gesamtpreis: ${amount * stock.price_per_stock}€`}</DialogContentText>
+                <DialogContentText className="tradingContent" sx={{marginTop: '10px'}}>{`Gesamtpreis: ${Number(amount * stock.price_per_stock).toFixed(2)}€`}</DialogContentText>
             }
             <div className="tradingActions">
                 <Button variant="contained" className="tradingButton" onClick={() => buyStock()}>Kaufen</Button>
