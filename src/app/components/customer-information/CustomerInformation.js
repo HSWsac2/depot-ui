c37310f49361c964576c1334e5ae4b5639388ae5
Jakob <j.wilmesmeier@gmail.com>
2022-03-11 10:10:42 +0100

    reload fix

--- src/app/components/customer-information/CustomerInformation.js
+++ src/app/components/customer-information/CustomerInformation.js
@@ -13,102 +13,104 @@ export default function CustomerInformation({customer, setCustomer}) {
 
     // TODO use userInformation    
     console.log("userInformation", userValue?.currentUser);
-    let user = userValue.currentUser;
+    let user = userValue?.currentUser;
     
     return <>
-        <div className="customerContainer">
-            <div className="customerIcon">
-                <AssignmentInd sx={{fontSize: 200}}/>
+        {user &&
+            <div className="customerContainer">
+                <div className="customerIcon">
+                    <AssignmentInd sx={{fontSize: 200}}/>
+                </div>
+                <div className="customerData">
+                    <Card className="customerDataCard">
+                        <Typography variant="h5" className="cardTitle">Kontaktdaten</Typography>
+                        <div className="cardContent">
+                            <TextField 
+                                variant="outlined" 
+                                label="Name" 
+                                disabled
+                                value={`${user.firstname} ${user.lastname}`}
+                                className="dataField"
+                                fullWidth
+                            />
+                            {/* <TextField 
+                                variant="outlined" 
+                                label="Mobilnummer"
+                                value={userValue.}
+                                className="dataField"
+                                fullWidth
+                                disabled
+                            />   */}
+                            <TextField 
+                                variant="outlined" 
+                                label="E-Mail"
+                                value={user.e_mail}
+                                className="dataField"
+                                fullWidth
+                                disabled
+                            />
+                        </div>
+                    </Card>
+                    {/* <Card className="customerDataCard">
+                        <Typography variant="h5" className="cardTitle">Kontodaten</Typography>
+                        <div className="cardContent">
+                            <TextField 
+                                variant="outlined" 
+                                label="Verrechnungskonto"
+                                value={customer.account}
+                                className="dataField"
+                                fullWidth
+                                disabled
+                            /> 
+                            <TextField 
+                                variant="outlined" 
+                                label="Kaufkraft"
+                                value={customer.buyingpower}
+                                className="dataField"
+                                fullWidth
+                                disabled
+                            />  
+                        </div>
+                    </Card> */}
+                    <Card className="customerDataCard">
+                        <Typography variant="h5" className="cardTitle">Persönliche Daten</Typography>
+                        <div className="cardContent">
+                            <TextField 
+                                variant="outlined" 
+                                label="Anschrift"
+                                value={`${user.street} ${user.house_number}`}
+                                className="dataField"
+                                fullWidth
+                                disabled
+                            /> 
+                            <TextField 
+                                variant="outlined" 
+                                label="Wohnort"
+                                value={`${user.plz} ${user.city}`}
+                                className="dataField"
+                                fullWidth
+                                disabled
+                            /> 
+                            <TextField 
+                                variant="outlined" 
+                                label="Geburtsdatum"
+                                value={user.birthdate}
+                                className="dataField"
+                                fullWidth
+                                disabled
+                            />  
+                            <TextField 
+                                variant="outlined" 
+                                label="Staatsangehörigkeit"
+                                value={user.nationality}
+                                className="dataField"
+                                fullWidth
+                                disabled
+                            />
+                        </div>
+                    </Card>
+                </div>
             </div>
-            <div className="customerData">
-                <Card className="customerDataCard">
-                    <Typography variant="h5" className="cardTitle">Kontaktdaten</Typography>
-                    <div className="cardContent">
-                        <TextField 
-                            variant="outlined" 
-                            label="Name" 
-                            disabled
-                            value={`${user.firstname} ${user.lastname}`}
-                            className="dataField"
-                            fullWidth
-                        />
-                        {/* <TextField 
-                            variant="outlined" 
-                            label="Mobilnummer"
-                            value={userValue.}
-                            className="dataField"
-                            fullWidth
-                            disabled
-                        />   */}
-                        <TextField 
-                            variant="outlined" 
-                            label="E-Mail"
-                            value={user.e_mail}
-                            className="dataField"
-                            fullWidth
-                            disabled
-                        />
-                    </div>
-                </Card>
-                {/* <Card className="customerDataCard">
-                    <Typography variant="h5" className="cardTitle">Kontodaten</Typography>
-                    <div className="cardContent">
-                        <TextField 
-                            variant="outlined" 
-                            label="Verrechnungskonto"
-                            value={customer.account}
-                            className="dataField"
-                            fullWidth
-                            disabled
-                        /> 
-                        <TextField 
-                            variant="outlined" 
-                            label="Kaufkraft"
-                            value={customer.buyingpower}
-                            className="dataField"
-                            fullWidth
-                            disabled
-                        />  
-                    </div>
-                </Card> */}
-                <Card className="customerDataCard">
-                    <Typography variant="h5" className="cardTitle">Persönliche Daten</Typography>
-                    <div className="cardContent">
-                        <TextField 
-                            variant="outlined" 
-                            label="Anschrift"
-                            value={`${user.street} ${user.house_number}`}
-                            className="dataField"
-                            fullWidth
-                            disabled
-                        /> 
-                        <TextField 
-                            variant="outlined" 
-                            label="Wohnort"
-                            value={`${user.plz} ${user.city}`}
-                            className="dataField"
-                            fullWidth
-                            disabled
-                        /> 
-                        <TextField 
-                            variant="outlined" 
-                            label="Geburtsdatum"
-                            value={user.birthdate}
-                            className="dataField"
-                            fullWidth
-                            disabled
-                        />  
-                        <TextField 
-                            variant="outlined" 
-                            label="Staatsangehörigkeit"
-                            value={user.nationality}
-                            className="dataField"
-                            fullWidth
-                            disabled
-                        />
-                    </div>
-                </Card>
-            </div>
-        </div>
+        }
     </>;
 }
