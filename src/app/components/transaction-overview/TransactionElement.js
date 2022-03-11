711bc6a60b9208074cf1fd9a3b1112f3a222dc5f
Jakob <j.wilmesmeier@gmail.com>
2022-03-11 11:57:32 +0100

    backend implementation transaction overview + missing fields

--- src/app/components/transaction-overview/TransactionElement.js
+++ src/app/components/transaction-overview/TransactionElement.js
@@ -1,54 +1,75 @@
-import BusinessIcon from '@mui/icons-material/Business';
-import { Avatar, Box, Divider, ListItem, ListItemAvatar, ListItemButton, Typography, useMediaQuery } from "@mui/material";
+import BusinessIcon from "@mui/icons-material/Business";
+import {
+	Avatar,
+	Box,
+	Divider,
+	ListItem,
+	ListItemAvatar,
+	ListItemButton,
+	Typography,
+	useMediaQuery,
+} from "@mui/material";
 import React, { useState } from "react";
 
-const currencyFormat = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' });
+const currencyFormat = new Intl.NumberFormat("de-DE", {
+	style: "currency",
+	currency: "EUR",
+});
 
 const TransactionElement = ({ transaction, isLast }) => {
+	// hide the icon on small devices
+	const bigDevice = useMediaQuery("(min-width:600px)");
 
-    // hide the icon on small devices
-    const bigDevice = useMediaQuery('(min-width:600px)');
+	const [open, setOpen] = useState(false);
 
-    const [open, setOpen] = useState(false);
-
-    return (
-        <>
-            <ListItem sx={{ padding: 0 }}>
-                <ListItemButton onClick={() => setOpen(true)}>
-                    {bigDevice && (
-                        <ListItemAvatar sx={{marginRight: '12px'}}>
-                            <Avatar>
-                                <BusinessIcon />
-                            </Avatar>
-                        </ListItemAvatar>)}
-                    <Box sx={{
-                        padding: '12px 32px 16px 12px',
-                        width: '100%'
-                    }}>
-                        <Box sx={{ display: 'flex', width: '100%' }}>
-                            <Typography variant='h6' sx={{ display: 'inline', flexGrow: 1, fontWeight: 'bold', marginRight: '12px' }}>
+	return (
+		<>
+			<ListItem sx={{ padding: 0 }}>
+				<ListItemButton onClick={() => setOpen(true)}>
+					{bigDevice && (
+						<ListItemAvatar sx={{ marginRight: "12px" }}>
+							<Avatar>
+								<BusinessIcon />
+							</Avatar>
+						</ListItemAvatar>
+					)}
+					<Box
+						sx={{
+							padding: "12px 32px 16px 12px",
+							width: "100%",
+						}}
+					>
+						<Box sx={{ display: "flex", width: "100%" }}>
+							{/* <Typography variant='h6' sx={{ display: 'inline', flexGrow: 1, fontWeight: 'bold', marginRight: '12px' }}>
                                 {transaction.name}
-                            </Typography>
-                            <Typography align='right' sx={{ display: 'inline', fontWeight: 'bold' }}>
-                                {currencyFormat.format(transaction.totalPrice)}
-                            </Typography>
-                        </Box>
+                            </Typography> */}
+							<Typography
+								align="right"
+								sx={{ display: "inline", fontWeight: "bold" }}
+							>
+								{currencyFormat.format(
+									transaction.buying_price *
+										transaction.piece_amt
+								)}
+							</Typography>
+						</Box>
 
-                        <Typography variant='subtitle1'>
+						{/* <Typography variant='subtitle1'>
                             {transaction.date}
-                        </Typography>
-                        <Typography variant='body1'>
-                            Anzahl: {transaction.count}
-                        </Typography>
-                        <Typography variant='body1'>
-                            Preis pro Stück: {currencyFormat.format(transaction.pricePerShare)}
-                        </Typography>
-                    </Box>
-                </ListItemButton>
-            </ListItem>
-            {!isLast && <Divider />}
-        </>
-    )
+                        </Typography> */}
+						<Typography variant="body1">
+							Anzahl: {transaction.piece_amt}
+						</Typography>
+						<Typography variant="body1">
+							Preis pro Stück:{" "}
+							{currencyFormat.format(transaction.buying_price)}
+						</Typography>
+					</Box>
+				</ListItemButton>
+			</ListItem>
+			{!isLast && <Divider />}
+		</>
+	);
 };
 
 export default TransactionElement;
--- src/app/components/transaction-overview/TransactionOverview.js
+++ src/app/components/transaction-overview/TransactionOverview.js
@@ -1,99 +1,130 @@
-import { Container, List } from "@mui/material";
-import React from "react";
-import TransactionElement from './TransactionElement';
+import { Alert, Container, List, Snackbar } from "@mui/material";
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { DepotContext } from "../../../context/DepotContext";
+import TransactionElement from "./TransactionElement";
 
 const TransactionOverview = () => {
+	const [transactions, setTransactions] = useState([]);
+	const [errorMsg, setErrorMsg] = useState("");
+	const [isError, setIsError] = useState(false);
 
-    const transactions = [
-        {
-            id: 1,
-            name: "Alphabet",
-            count: 4,
-            pricePerShare: 2350,
-            totalPrice: -9400,
-            date: '06.12.2021',
-        },
-        {
-            id: 2,
-            name: "Meta",
-            count: 1,
-            pricePerShare: 192,
-            totalPrice: -192,
-            date: '01.01.2021',
-        },
-        {
-            id: 3,
-            name: "Amazon.com, Inc.",
-            count: 1,
-            pricePerShare: 2672,
-            totalPrice: -2672,
-            date: '03.07.2021',
-        },
-        {
-            id: 4,
-            name: "Alphabet",
-            count: 4,
-            pricePerShare: 2350,
-            totalPrice: -9400,
-            date: '06.12.2021',
-        },
-        {
-            id: 5,
-            name: "Meta",
-            count: 1,
-            pricePerShare: 192,
-            totalPrice: -192,
-            date: '01.01.2021',
-        },
-        {
-            id: 6,
-            name: "Amazon.com, Inc.",
-            count: 1,
-            pricePerShare: 2672,
-            totalPrice: -2672,
-            date: '03.07.2021',
-        },
-        {
-            id: 7,
-            name: "Alphabet",
-            count: 4,
-            pricePerShare: 2350,
-            totalPrice: -9400,
-            date: '06.12.2021',
-        },
-        {
-            id: 8,
-            name: "Meta",
-            count: 1,
-            pricePerShare: 192,
-            totalPrice: -192,
-            date: '01.01.2021',
-        },
-        {
-            id: 9,
-            name: "Amazon.com, Inc.",
-            count: 1,
-            pricePerShare: 2672,
-            totalPrice: -2672,
-            date: '03.07.2021',
-        }
-    ]
+	const { currentDepot } = useContext(DepotContext);
+	useEffect(() => {
+		const fetchTransactions = async () => {
+			axios
+				.get(
+					`http://localhost:8081/depots/${currentDepot.position_id}/${currentDepot.position_sub_id}/orderHistory`
+				)
+				.then((res) => {
+					setTransactions(res.data);
+				});
+		};
+		if (currentDepot) {
+			fetchTransactions();
+		} else {
+			setErrorMsg("Bitte zunächst ein Depot auswählen!");
+			setIsError(true);
+		}
+	}, []);
 
-    return <Container
-        maxWidth="lg"
-        spacing={0}
-        sx={{ marginTop: '2rem' }}
-    >
-        <List>
-            {transactions.map((transaction, index) => (
-                <TransactionElement
-                    key={transaction.id}
-                    transaction={transaction}
-                    isLast={index === transactions.length - 1}
-                />
-            ))}
-        </List>
-    </Container>
+	// const transactions = [
+	// 	{
+	// 		id: 1,
+	// 		name: "Alphabet",
+	// 		count: 4,
+	// 		pricePerShare: 2350,
+	// 		totalPrice: -9400,
+	// 		date: "06.12.2021",
+	// 	},
+	// 	{
+	// 		id: 2,
+	// 		name: "Meta",
+	// 		count: 1,
+	// 		pricePerShare: 192,
+	// 		totalPrice: -192,
+	// 		date: "01.01.2021",
+	// 	},
+	// 	{
+	// 		id: 3,
+	// 		name: "Amazon.com, Inc.",
+	// 		count: 1,
+	// 		pricePerShare: 2672,
+	// 		totalPrice: -2672,
+	// 		date: "03.07.2021",
+	// 	},
+	// 	{
+	// 		id: 4,
+	// 		name: "Alphabet",
+	// 		count: 4,
+	// 		pricePerShare: 2350,
+	// 		totalPrice: -9400,
+	// 		date: "06.12.2021",
+	// 	},
+	// 	{
+	// 		id: 5,
+	// 		name: "Meta",
+	// 		count: 1,
+	// 		pricePerShare: 192,
+	// 		totalPrice: -192,
+	// 		date: "01.01.2021",
+	// 	},
+	// 	{
+	// 		id: 6,
+	// 		name: "Amazon.com, Inc.",
+	// 		count: 1,
+	// 		pricePerShare: 2672,
+	// 		totalPrice: -2672,
+	// 		date: "03.07.2021",
+	// 	},
+	// 	{
+	// 		id: 7,
+	// 		name: "Alphabet",
+	// 		count: 4,
+	// 		pricePerShare: 2350,
+	// 		totalPrice: -9400,
+	// 		date: "06.12.2021",
+	// 	},
+	// 	{
+	// 		id: 8,
+	// 		name: "Meta",
+	// 		count: 1,
+	// 		pricePerShare: 192,
+	// 		totalPrice: -192,
+	// 		date: "01.01.2021",
+	// 	},
+	// 	{
+	// 		id: 9,
+	// 		name: "Amazon.com, Inc.",
+	// 		count: 1,
+	// 		pricePerShare: 2672,
+	// 		totalPrice: -2672,
+	// 		date: "03.07.2021",
+	// 	},
+	// ];
+
+	return (
+		<>
+			<Container maxWidth="lg" spacing={0} sx={{ marginTop: "2rem" }}>
+				<List>
+					{transactions.map((transaction, index) => (
+						<TransactionElement
+							key={transaction.transaction_id}
+							transaction={transaction}
+							isLast={index === transactions.length - 1}
+						/>
+					))}
+				</List>
+			</Container>
+			<Snackbar
+				open={isError}
+				autoHideDuration={2000}
+				onClose={() => setIsError(false)}
+			>
+				<Alert severity="error">{errorMsg}</Alert>
+			</Snackbar>
+		</>
+	);
 };
 
 export default TransactionOverview;
