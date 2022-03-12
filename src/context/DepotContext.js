b06b60aa84fc699b224ef1876dab315f12b09622
Lars Kecker <lars.kecker2009@gmail.com>
2022-03-12 11:43:15 +0100

    remove useless depot/user hooks

--- src/context/DepotContext.js
+++ src/context/DepotContext.js
@@ -1,14 +1,57 @@
-import { createContext, useContext } from "react";
-import useDepot from "../app/hooks/useDepot";
+import axios from "axios";
+import { createContext, useCallback, useContext, useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
 import { UserContext } from "./UserContext";
 
 export const DepotContext = createContext(null);
 
+const positionIdcookieName = 'positionId';
+const positionSubIdcookieName = 'positionSubId';
+
+const defaultCookieOptions = { path: "/", maxAge: 600, sameSite: 'lax' };
+
 export const DepotContextProvider = ({ children }) => {
 
-    const { currentUser } = useContext(UserContext);
+    const { currentUser } = useContext(UserContext)
+    
+    const [currentDepot, setCurrentDepot] = useState(null)
+    
+    // get initial deposit based on cookie values
+    const [depotCookie, setDepotCookie, removeDepotCookie] = useCookies(['depot'])
+    useEffect(() => {
+        const { positionId, positionSubId } = depotCookie;
+        if (positionId == null || positionSubId == null || currentUser.client_id == null) return;
+        axios.get(process.env.REACT_APP_BACKEND_URL_DEPOT_SERVICE + `depots/${positionId}/${positionSubId}`)
+            .then(response => response.data)
+            .then(response => setCurrentDepot(response))
+            .catch(console.error)
 
-    const depotValue = useDepot(currentUser?.client_id);
+        // the dependency array is intentionally left empty - the initial fetch should not reaccur when the cookie or user
+        // changes as those changes are usually accompanied by a dedicated fetch, so fetching the data again would be wasteful
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+
+    // define common functions to be used elsewhere
+    const selectDepot = useCallback((depot, rememberDepot) => {
+        setCurrentDepot(depot);
+        if (rememberDepot) {
+            setDepotCookie(positionIdcookieName, depot.position_id, defaultCookieOptions)
+            setDepotCookie(positionSubIdcookieName, depot.position_sub_id, defaultCookieOptions)
+        }
+    }, [setCurrentDepot, setDepotCookie])
+
+    const deselectDepot = useCallback(() => {
+        setCurrentDepot(null);
+        removeDepotCookie(positionIdcookieName, defaultCookieOptions);
+        removeDepotCookie(positionSubIdcookieName, defaultCookieOptions);
+    }, [setCurrentDepot, removeDepotCookie]);
+
+    const depotValue = {
+        currentDepot,
+        selectDepot,
+        deselectDepot,
+    }
 
     return (
         <DepotContext.Provider value={depotValue}>
--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,12 +1,38 @@
-import { createContext } from "react";
-import useUser from "../app/hooks/useUser";
+import { createContext, useCallback, useState } from "react";
+import { useCookies } from "react-cookie";
 
 export const UserContext = createContext(null);
 
+const cookieName = 'user';
+const defaultCookieOptions = { path: "/", maxAge: 600, sameSite: 'lax' };
+
 export const UserContextProvider = ({ children }) => {
 
-    const userValue = useUser();
+    const [userCookie, setUserCookie, removeUserCookie] = useCookies(['user']);
 
+    const [currentUser, setCurrentUser] = useState(userCookie.user ?? null)
+
+    const logout = useCallback(() => {
+        setCurrentUser(null)
+        removeUserCookie(cookieName, defaultCookieOptions);
+    }, [setCurrentUser, removeUserCookie]);
+
+    const login = useCallback((user, rememberUser) => {
+        setCurrentUser(user)
+        if (rememberUser) {
+            setUserCookie(cookieName, user, defaultCookieOptions);
+        } else {
+            removeUserCookie(cookieName, defaultCookieOptions);
+        }
+    }, [setCurrentUser, setUserCookie, removeUserCookie])
+
+    const userValue = {
+        currentUser,
+        logout,
+        login,
+    }
+
+    
     return (
         <UserContext.Provider value={userValue}>
             {children}
