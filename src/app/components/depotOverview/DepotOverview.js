52418d3fbb5678894fc2b9884c59d4a008597e8d
Sophie Mümken <sophie.muemken@icloud.com>
2022-03-18 10:36:54 +0100

    Versuch, Backend für Einzelaktien anzubinden, konnte es leider wegen Fehlern noch nicht testen

--- src/app/components/depotOverview/DepotOverview.js
+++ src/app/components/depotOverview/DepotOverview.js
@@ -18,8 +18,9 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import './DepotOverview.css';
 import { Grid } from '@mui/material';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { DepotContext } from '../../../context/DepotContext';
+import axios from 'axios';
 
 ChartJS.register(
     CategoryScale,
@@ -34,18 +35,29 @@ ChartJS.register(
 export default function DepotOverview() {
 
     const labels = ['5', '10', '15', '20', '25', '30'];
-    /*const depot = {
-        "position_id": "1234",
-        "position_sub_id": "5678",
-        "client_id": 1234,
-        "buying_power": 500,
-        "balance_amt": 20000
-    };*/
+    const [stocks, setStocks] = useState([]);
 
     //TODO hier entstehen noch Probleme, da das Depot anfangs null ist
     const { currentDepot } = useContext(DepotContext);
 
-    const positions = [
+    useEffect(() => {
+		const fetchTransactions = async () => {
+			axios
+				.get(
+					`${process.env.REACT_APP_BACKEND_URL_TRANSACTION_SERVICE}depots/${currentDepot.position_id}/${currentDepot.position_sub_id}/currentStocks`
+				)
+				.then((res) => {
+					setStocks(res.data);
+				});
+		};
+		if (currentDepot) {
+			fetchTransactions();
+		} else {
+			setStocks([]);
+		}
+	}, [currentDepot]);
+
+    /*const positions = [
         {
             name: "Tesla",
             amount: 3.8,
@@ -70,7 +82,7 @@ export default function DepotOverview() {
             buyingPrice: 500.78,
             currentPrice: 300.0
         },
-    ]
+    ]*/
 
     //const values = [];
 
@@ -111,7 +123,7 @@ export default function DepotOverview() {
                         <Table sx={{ minWidth: 650 }} aria-label="simple table">
                             <TableHead>
                                 <TableRow>
-                                    <TableCell>Name</TableCell>
+                                    <TableCell>ISIN</TableCell>
                                     <TableCell align="right">Anzahl</TableCell>
                                     <TableCell align="right">Einkaufspreis</TableCell>
                                     <TableCell align="right">Aktueller Preis</TableCell>
@@ -119,18 +131,18 @@ export default function DepotOverview() {
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                {positions.map((row) => (
+                                {stocks.map((row) => (
                                     <TableRow
-                                        key={row.name}
+                                        key={row.isin}
                                         sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                     >
                                         <TableCell component="th" scope="row">
-                                            {row.name}
+                                            {row.isin}
                                         </TableCell>
-                                        <TableCell align="right">{row.amount}</TableCell>
-                                        <TableCell align="right">{row.buyingPrice}€</TableCell>
-                                        <TableCell align="right">{row.currentPrice}€</TableCell>
-                                        <TableCell align="right">{(row.currentPrice - row.buyingPrice) < 0 ? "-" + Number((row.currentPrice * 100 / row.buyingPrice)).toFixed(2) +"%" : "+" + Number((row.currentPrice * 10 / row.buyingPrice)).toFixed(2)+"%"}</TableCell>
+                                        <TableCell align="right">{row.piece_amt}</TableCell>
+                                        <TableCell align="right">{row.buying_price}€</TableCell>
+                                        <TableCell align="right">{row.current_price}€</TableCell>
+                                        <TableCell align="right">{row.growth_rate < 0 ? "-" + row.growth_rate +"%" : "+" + row.growth_rate +"%"}</TableCell>
                                     </TableRow>
                                 ))}
                             </TableBody>
