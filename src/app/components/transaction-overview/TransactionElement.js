c7b0cf39faafdcd3f2cb3db7dcfaf29d1150e37a
Lars Kecker <lars.kecker2009@gmail.com>
2022-03-25 17:29:50 +0100

    make TransactionElement not clickable
    also added a minus to price on buy

--- src/app/components/transaction-overview/TransactionElement.js
+++ src/app/components/transaction-overview/TransactionElement.js
@@ -1,16 +1,14 @@
 import BusinessIcon from "@mui/icons-material/Business";
-import moment from "moment";
 import {
 	Avatar,
 	Box,
 	Divider,
 	ListItem,
-	ListItemAvatar,
-	ListItemButton,
-	Typography,
-	useMediaQuery,
+	ListItemAvatar, Typography,
+	useMediaQuery
 } from "@mui/material";
-import React, { useState } from "react";
+import moment from "moment";
+import React from "react";
 
 const currencyFormat = new Intl.NumberFormat("de-DE", {
 	style: "currency",
@@ -21,69 +19,62 @@ const TransactionElement = ({ transaction, isLast }) => {
 	// hide the icon on small devices
 	const bigDevice = useMediaQuery("(min-width:600px)");
 
-	const [open, setOpen] = useState(false);
-
 	return (
 		<>
 			<ListItem sx={{ padding: 0 }}>
-				<ListItemButton onClick={() => setOpen(true)}>
-					{bigDevice && (
-						<ListItemAvatar sx={{ marginRight: "12px" }}>
-							<Avatar>
-								<BusinessIcon />
-							</Avatar>
-						</ListItemAvatar>
-					)}
-					<Box
-						sx={{
-							padding: "12px 32px 16px 12px",
-							width: "100%",
-						}}
-					>
-						<Box sx={{ display: "flex", width: "100%" }}>
-							<Typography
-								variant="h6"
-								sx={{
-									display: "inline",
-									flexGrow: 1,
-									fontWeight: "bold",
-									marginRight: "12px",
-								}}
-							>
-								{transaction.name}
-							</Typography>
-							<Typography
-								align="right"
-								sx={{
-									display: "inline",
-									fontWeight: "bold",
-									color: transaction.sold ? "black" : "red",
-								}}
-							>
-								{currencyFormat.format(
-									transaction.buying_price *
-										transaction.piece_amt
-								)}
-							</Typography>
-						</Box>
-
-						<Typography variant="subtitle1">
-							{moment(transaction.keydate, "YYYY-MM-DD").format(
-								"DD.MM.YYYY"
+				{bigDevice && (
+					<ListItemAvatar sx={{ marginRight: "12px" }}>
+						<Avatar>
+							<BusinessIcon />
+						</Avatar>
+					</ListItemAvatar>
+				)}
+				<Box
+					sx={{
+						padding: "12px 32px 16px 12px",
+						width: "100%",
+					}}
+				>
+					<Box sx={{ display: "flex", width: "100%" }}>
+						<Typography
+							variant="h6"
+							sx={{
+								display: "inline",
+								flexGrow: 1,
+								fontWeight: "bold",
+								marginRight: "12px",
+							}}
+						>
+							{transaction.name}
+						</Typography>
+						<Typography
+							align="right"
+							sx={{
+								display: "inline",
+								fontWeight: "bold",
+								color: transaction.sold ? "black" : "red",
+							}}
+						>
+							{currencyFormat.format(
+								(transaction.sold ? 1 : -1) * 
+								transaction.buying_price *
+								transaction.piece_amt
 							)}
 						</Typography>
-						<Typography variant="body1">
-							Anzahl:{" "}
-							{`${transaction.sold ? "-" : ""}${
-								transaction.piece_amt
-							}`}
-						</Typography>
-						<Typography variant="body1">
-							Preis pro Stück:{" "}
-							{currencyFormat.format(transaction.buying_price)}
-						</Typography>
 					</Box>
-				</ListItemButton>
+
+					<Typography variant="subtitle1">
+						{moment(transaction.keydate, "YYYY-MM-DD").format("DD.MM.YYYY")}
+					</Typography>
+					<Typography variant="body1">
+						Anzahl:{" "}
+						{`${transaction.sold ? "-" : ""}${transaction.piece_amt}`}
+					</Typography>
+					<Typography variant="body1">
+						Preis pro Stück:{" "}
+						{currencyFormat.format(transaction.buying_price)}
+					</Typography>
+				</Box>
 			</ListItem>
 			{!isLast && <Divider />}
 		</>
